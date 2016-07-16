f32fc9860e2be991a270bc8c77833bf14503901f
Gabriel Alves <itsmealves@gmail.com>
2016-07-16 18:49:57 -0300

    add openBodyReader

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ function handle(bodyFrame) {
 
 }
 
-if(kinect.open()) {
+if(kinect && kinect.open()) {
     console.log("Kinect opened!");
     kinect.on("bodyFrame", handle);
 }
 else {
     console.log("Something went wrong :/");
 }
+
+if(kinect)
+    kinect.openBodyReader();
