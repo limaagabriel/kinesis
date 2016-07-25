0ce9b860cfc6791659aa04d1c99127300921a504
Gabriel Alves <itsmealves@gmail.com>
2016-07-25 15:01:43 -0300

    Add safe angle adjustment

--- Kinesis/MainWindow.xaml.cs
+++ Kinesis/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Research.Kinect.Nui;
+using Kinect.Extensions;
 
 namespace Kinesis
 {
@@ -21,48 +22,39 @@ namespace Kinesis
     /// </summary>
     public partial class MainWindow : Window
     {
-        Runtime kinectRuntime = new Runtime();
+        Runtime kinect = new Runtime();
 
         public MainWindow()
         {
             InitializeComponent();
-            kinectRuntime.Initialize(RuntimeOptions.UseSkeletalTracking);
-            kinectRuntime.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            kinect.Initialize(RuntimeOptions.UseSkeletalTracking);
+            kinect.NuiCamera.TryToSetAngle(0);
+            kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            kinect.NuiCamera.TryToSetAngle(20);
         }
 
         void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
-            // Pega todos os 6 esqueletos
             SkeletonFrame allSkeletons = e.SkeletonFrame;
-
-            // Pegamos apenas o primeiro com o status de "Tracked"
             SkeletonData skeleton = (from s in allSkeletons.Skeletons
                                      where s.TrackingState == SkeletonTrackingState.Tracked
                                      select s).FirstOrDefault();
 
-            // Limpa todos os items do canvas
             cnArea.Children.Clear();
 
-            // Cria um loop com todas as juntas do esqueleto
             if (skeleton != null)
             {
                 foreach (Joint item in skeleton.Joints)
                 {
-                    // Cria uma
                     Ellipse objEllipse = new Ellipse();
-                    // Adiciona uma cor
                     objEllipse.Fill = new SolidColorBrush(Color.FromArgb(150, 55, 255, 0));
-                    // Tamanho
                     objEllipse.Width = 20;
                     objEllipse.Height = 20;
-                    // Adiciona ao Canvas
                     cnArea.Children.Add(objEllipse);
-                    // Converte a posição da junta e altera a posição da ellipse
                     setDisplayPosition(ref objEllipse, item);
                 }
             }
         }
-
 
         public void setDisplayPosition(ref Ellipse ellipse, Joint joint)
         {
@@ -70,7 +62,7 @@ namespace Kinesis
 
             float depthX, depthY;
             // Converte a posição de Skeleton para DepthImage que gera dois floats entre 0.0 e 1.0
-            this.kinectRuntime.SkeletonEngine.SkeletonToDepthImage(joint.Position, out depthX, out depthY);
+            this.kinect.SkeletonEngine.SkeletonToDepthImage(joint.Position, out depthX, out depthY);
 
             // Converte para 320 / 240 que é o que o GetColorPixelCoordinatesFromDepthPixel suporta
             depthX = Math.Max(0, Math.Min(depthX * 320, 320));
@@ -79,23 +71,16 @@ namespace Kinesis
             ImageViewArea imageView = new ImageViewArea();
 
             // Converte os valores de DepthPixel para ColorPixel, que agora sim é o que precisamos
-            this.kinectRuntime.NuiCamera.GetColorPixelCoordinatesFromDepthPixel(ImageResolution.Resolution640x480,
+            this.kinect.NuiCamera.GetColorPixelCoordinatesFromDepthPixel(ImageResolution.Resolution640x480,
                 imageView, (int)depthX, (int)depthY, 0, out colourX, out colourY);
 
-            // Agora você ja tem um valor mais exato das coordenadas
             Canvas.SetLeft(ellipse, colourX);
             Canvas.SetTop(ellipse, colourY);
-
-            // Você tem a opção de escalar ela para o tamanho do seu esqueleto também
-            // Canvas.SetLeft(ellipse, cnArea.Width * colourX / 640 - 20);
-            // Canvas.SetTop(ellipse, cnArea.Height * colourY / 480 - 20);           
-
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            // Desliga o Kinect
-            kinectRuntime.Uninitialize();
+            kinect.Uninitialize();
         }
     }
 }
---
+++ Kinesis/config/AngleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Research.Kinect.Nui;
+
+namespace Kinect.Extensions
+{
+    public static class CameraExtensions
+    {
+        public static bool TryToSetAngle(this Camera cam, int angle)
+        {
+            try
+            {
+                cam.ElevationAngle = angle;
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
